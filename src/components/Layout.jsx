7ae86ee06607caebbdad6e148ef1a68a983dd2e5
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-29 22:00:55 +0500

    updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,8 +9,9 @@ import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  SearchOutlined
 } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme, Input } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 const siderStyle = {
   overflow: 'auto',
@@ -73,12 +74,15 @@ const MyLayout = ({ children }) => {
         >
           <h4> SpeakUp  </h4>
 
+          <Input prefix={<SearchOutlined />} type="search" placeholder="Search" className='search'/>
 
 
           <button type="button" class="button ms-5">
             <span class="button__text">Add Post</span>
             <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg"><line y2="19" y1="5" x2="12" x1="12"></line><line y2="12" y1="12" x2="19" x1="5"></line></svg></span>
           </button>
+
+
         </Header>
         <Content
           style={{
